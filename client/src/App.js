927dfde97753afe2ec951f7b13205c8d2291dd9a
AdrianaProvenzano <adriana.prove97@gmail.com>
2021-11-09 12:18:22 +0100

    added NavbarComponent and routes structure in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Alert, Button, Spinner } from "react-bootstrap";
 import { getEmployeeByID } from "./services/ApiClient";
+import { NavbarComponent } from "./ui-components/NavbarComponent";
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   const [employee, setEmployee] = useState({});
@@ -16,21 +18,54 @@ function App() {
   }
 
   return (
-    <Container>
-      <Row className='vh-100 justify-content-center align-items-center'>
-        <Col className='d-flex justify-content-center align-items-center flex-column'>
-          {isLoading ? (
-            <Spinner animation='border' variant='primary' />
-          ) : (
-            <>
-              <Button onClick={onButtonClick}>Get an employee name!</Button>
-              <h1>{employee.fullName}</h1>
-              {error ? <Alert>{error}</Alert> : null}
-            </>
-          )}
-        </Col>
-      </Row>
-    </Container>
+    <Router>
+      <Container>
+        <Row>
+          <NavbarComponent cart={1} userType={1} />   
+          {/*cart prop receives the length of the items list in shopping cart
+          userType prop receives 0 if the user is an employee, 1 a client */}
+        </Row>
+        <Row className='vh-100 justify-content-center align-items-center'>
+
+          <Switch>
+            <Route exact path="/">
+              <h1>Home page</h1>
+              <Col className='d-flex justify-content-center align-items-center flex-column'>
+                {isLoading ? (
+                  <Spinner animation='border' variant='primary' />
+                ) : (
+                  <>
+                    <Button onClick={onButtonClick}>Get an employee name!</Button>
+                    <h1>{employee.fullName}</h1>
+                    {error ? <Alert>{error}</Alert> : null}
+                  </>
+                )}
+              </Col>
+            </Route>
+            <Route exact path="/cart">
+              <h1>Cart page</h1>
+            </Route>
+            <Route exact path="/orders">
+              <h1>Orders page</h1>
+            </Route>
+            <Route exact path="/clients">
+              <h1>Clients page</h1>
+            </Route>
+            <Route exact path="/neworder">
+              <h1>New order page</h1>
+            </Route>
+            <Route exact path="/newclient">
+              <h1>New client page</h1>
+            </Route>
+            <Route path="/">
+              <Redirect to="/" />
+            </Route>
+
+          </Switch>
+        </Row>
+      </Container>
+    </Router>
+
   );
 }
 

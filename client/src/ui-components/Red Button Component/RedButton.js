a5e7588fdd6283c28a140ca6ee0cb62e002c7d0f
MrGreensushi <danspi@live.it>
2021-11-11 19:36:36 +0100

    RedButton css

--- client/src/ui-components/Red Button Component/RedButton.js
+++ client/src/ui-components/Red Button Component/RedButton.js
@@ -1,27 +1,44 @@
 import React from "react";
-import { Button, DropdownButton,Dropdown } from "react-bootstrap";
+import { Button, DropdownButton, Dropdown, ButtonGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { routes, shoppingCartRouteName } from "../../Routes";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./RedButton.css";
+import DropdownToggle from "@restart/ui/esm/DropdownToggle";
 
 function RedButton(props) {
-    return (
-        <Button className="button">{props.text}</Button>
-    );
-
+  return <Button className="button">{props.text}</Button>;
 }
 //passo un vettore stringa dove ogni stringa è un item del drop Down
 function RedButtonDropDown(props) {
-    return (
-        <DropdownButton className="dropbtn "  title={props.title}>
-            {
-                props.items.map((name, index) => <Dropdown.Item className="dropdown-content"  key={index}> {name} </Dropdown.Item>)
-            }
-        </DropdownButton>
-    );
+  /*return (
+    <DropdownButton as={ButtonGroup} title={props.title} className="dropbtn">
+      {props.items.map((name, index) => (
+        <Dropdown.Item className="dropdown-content" key={index}>
+          {" "}
+          {name}{" "}
+        </Dropdown.Item>
+      ))}
+    </DropdownButton>
+  );*/
 
+  return (
+    <Dropdown as={ButtonGroup}>
+      <Dropdown.Toggle id="dropdown-custom-1" className="button">
+        {props.title}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {" "}
+        {props.items.map((name, index) => (
+          <Dropdown.Item className="dropdown-content" key={index}>
+            {" "}
+            {name}{" "}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 }
 
-export {RedButton, RedButtonDropDown}; 
+export { RedButton, RedButtonDropDown };

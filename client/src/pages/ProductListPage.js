f00db313b57834084af7e83de6fce556542387fb
Daniele Spina s285377 <danspi@live.it>
2021-11-12 14:59:16 +0100

    Moved product card and page to the rigth folders

--- client/src/pages/ProductListPage.js
+++ client/src/pages/ProductListPage.js
@@ -1,19 +1,39 @@
 import { React, useState } from "react";
-import { Container, Row, Col, Dropdown, Form, Button, CardGroup, FormControl, DropdownButton } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Dropdown,
+  Form,
+  Button,
+  CardGroup,
+  FormControl,
+  DropdownButton,
+} from "react-bootstrap";
 import { employeeNavbarLinks } from "../Routes";
 import { NavbarComponent } from "../ui-components/NavbarComponent/NavbarComponent";
 import Product from "../services/models/Product";
-import ProductCard from "../ui-components/ProductCard"
-import { RedButton, RedButtonDropDown } from "../ui-components/Red Button Component/RedButton"
+import ProductCard from "../ui-components/ProductCard";
+import {
+  RedButton,
+  RedButtonDropDown,
+} from "../ui-components/Red Button Component/RedButton";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function ProductListPage(props) {
-
   /* TODO: Retrieve this products from back-end*/
-  const [products, setProducts] = useState([new Product(1, "giovanni", "Zucchini", "Origin: Italy", "vegetables"),
-  new Product(2, "Luca", "Bananas", "Origin: Italy", "vegetables")]);
+  const [products, setProducts] = useState([
+    new Product(1, "giovanni", "Zucchini", "Origin: Italy", "vegetables"),
+    new Product(2, "Luca", "Bananas", "Origin: Italy", "vegetables"),
+    new Product(3, "Luca", "Bananas", "Origin: Italy", "vegetables"),
+    new Product(4, "Luca", "Bananas", "Origin: Italy", "vegetables"),
+    new Product(5, "Luca", "Bananas", "Origin: Italy", "vegetables"),
+  ]);
 
+  const [ids,setIds]=useState();
+  const [category, setCategory]= useState();
+  const [searcString, setSearchString]= useState();
 
   return (
     <Container>
@@ -21,48 +41,46 @@ function ProductListPage(props) {
         <NavbarComponent
           links={employeeNavbarLinks}
           showShoppingCart
-          shoppingCartItems='1'
+          shoppingCartItems="1"
         />
       </Row>
 
-      <Row>
-        <Container>
+      <Row fluid>
+        <Row>
           <h1>Available products</h1>
+        </Row>
+        <Row>
+          <Col>
+            <RedButtonDropDown
+              items={Object.values(Product.Categories)}
+              title={"Categories"}
+              onClick={}
+            />
+          </Col>
 
-          <Row >
-            <Col>
-
-              <RedButtonDropDown items={Object.values(Product.Categories)} title={"Categories"} />
-
-            </Col>
-
-            <Col>
-              <Form>
-                <Row>
-                  <Col>
-                    <FormControl type="text" placeholder="Filter" />
-                  </Col>
-                  <Col>
-                    <RedButton text="Search" />
-                  </Col>
-                </Row>
-              </Form>
-            </Col>
-          </Row>
-
-          <Row>
-            {
-              products.map((item, index) => {
-                return (
-                  <CardGroup as={Col}>
-                    <ProductCard product={item} />
-                  </CardGroup>
-                );
-              })
-            }
-
-          </Row>
-        </Container>
+          <Col>
+            <Form>
+              <Row>
+                <Col>
+                  <FormControl type="text" placeholder="Filter" />
+                </Col>
+                <Col>
+                  <RedButton text="Search" />
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+        </Row>
+        <hr />
+        <Row md={4} xs={2} className="g-4">
+          {products.map((item) => {
+            return (
+              <CardGroup as={Col}>
+                <ProductCard product={item} />
+              </CardGroup>
+            );
+          })}
+        </Row>
       </Row>
     </Container>
   );
--- client/src/ui-components/Red Button Component/RedButton.js
+++ client/src/ui-components/Red Button Component/RedButton.js
@@ -8,27 +8,21 @@ import "./RedButton.css";
 import DropdownToggle from "@restart/ui/esm/DropdownToggle";
 
 function RedButton(props) {
-  return <Button className="button">{props.text}</Button>;
+  return (
+    <Button className="button" onClick={props.onClick}>
+      {props.text}
+    </Button>
+  );
 }
+
 //passo un vettore stringa dove ogni stringa è un item del drop Down
 function RedButtonDropDown(props) {
-  /*return (
-    <DropdownButton as={ButtonGroup} title={props.title} className="dropbtn">
-      {props.items.map((name, index) => (
-        <Dropdown.Item className="dropdown-content" key={index}>
-          {" "}
-          {name}{" "}
-        </Dropdown.Item>
-      ))}
-    </DropdownButton>
-  );*/
-
   return (
     <Dropdown as={ButtonGroup}>
-      <Dropdown.Toggle id="dropdown-custom-1" className="button">
+      <Dropdown.Toggle id="dropdown-custom-1" className="dropdown">
         {props.title}
       </Dropdown.Toggle>
-      <Dropdown.Menu>
+      <Dropdown.Menu className="dropdown-menu">
         {" "}
         {props.items.map((name, index) => (
           <Dropdown.Item className="dropdown-content" key={index}>

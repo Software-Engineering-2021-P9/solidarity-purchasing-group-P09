5ae04736f9e62750a77efb483286542ffca2af8f
AdrianaProvenzano <adriana.prove97@gmail.com>
2021-11-13 19:48:14 +0100

    deleted front end part

--- client/src/pages/ClientDetailsPage.js
+++ client/src/pages/ClientDetailsPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import { NavbarComponent } from "../ui-components/NavbarComponent/NavbarComponent";
-import { CreateNewOrderButton } from "../ui-components/ClientDetailsComponent/CreateNewOrderButton";
 import { employeeNavbarLinks } from "../Routes";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,27 +13,6 @@ function ClientDetailsPage(props) {
       </Row>
       <Row>
         <h1>ClientDetailsPage</h1>
-      </Row>
-      {/* this part is only for debug pruposes 
-      { props.location.state != null ? (
-        <>
-          <Row>
-            <p>
-              Order is recorded to database!! Client with id:
-              {props.location.state.clientId}{" "}
-            </p>
-          </Row>
-          <Row>
-            <CreateNewOrderButton clientId={props.location.state.clientId} />
-          </Row>
-        </>
-      ) : (
-        <Row>
-          <CreateNewOrderButton />
-      </Row>
-       )} */}
-      <Row>
-        <CreateNewOrderButton />
       </Row>
     </Container>
   );
--- client/src/pages/ProductListPage.js
+++ client/src/pages/ProductListPage.js
@@ -6,10 +6,6 @@ import { NavbarComponent } from "../ui-components/NavbarComponent/NavbarComponen
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function ProductListPage(props) {
-  if (props.location.state != null) {
-    console.log(props.location.state.cart);
-  }
-
   return (
     <Container>
       <Row>
@@ -18,24 +14,10 @@ function ProductListPage(props) {
           showShoppingCart
           shoppingCartItems="1"
         />
-        {/*pass props.clientId*/}
       </Row>
       <Row>
         <h1>ProductListPage</h1>
       </Row>
-      {/* this part is only for debug pruposes 
-      {props.location.state != null ? (
-        <>
-          <Row>
-            <p>
-              I'm creating an order for the client with id:
-              {props.location.state.clientId}{" "}
-            </p>
-          </Row>
-        </>
-      ) : (
-        ""
-      )} */}
     </Container>
   );
 }

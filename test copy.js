62646de27e165e9cc14906ec5b6e73e003909a2d
G-Salviati <giovannisalviati@gmail.com>
2021-11-14 16:42:13 +0100

    merged test

--- test copy.js
+++ test copy.js
@@ -2,6 +2,7 @@ let chai = require("chai");
 let expect = chai.expect;
 let chaiHttp = require("chai-http");
 let dao = require("../dao/dao");
+const { ObjectID } = require("bson");
 chai.use(chaiHttp);
 
 // This will contain the main server app, needed to listen for requests.
@@ -11,6 +12,7 @@ let app;
 // Init mock MongoDB
 const mongoUnit = require("mongo-unit");
 let testData = require("./test-data");
+
 
 const mongoTestDBName = "spg-test";
 mongoUnit.start({ dbName: mongoTestDBName }).then(() => {
@@ -173,158 +175,6 @@ describe("Employees API tests:", () => {
           expect(err).to.be.null;
           expect(res.status).to.be.equal(500);
 
-          done();
-        });
-    });
-  });
-});
-
-describe("Products API tests: ", () => {
-  beforeEach(() => {
-    dao.open();
-    mongoUnit.load(testData.productsCollection);
-    dao.createProductsTextSearchIndexes();
-  });
-
-  afterEach(() => {
-    mongoUnit.drop();
-    dao.close();
-  });
-
-  describe("GET /products", () => {
-    it("it should retrieve the list of products based on the filters sent: CATEGORY", (done) => {
-      chai
-        .request(app)
-        .get("/api/products?category=meat")
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res.status).to.be.equal(200);
-          expect(res.body).to.be.an("array");
-          expect(res.body.length).to.be.equal(2);
-          expect(
-            res.body.filter((product) => product.category == "meat").length
-          ).to.be.equal(2);
-          done();
-        });
-    });
-
-    it("it should retrieve the list of products based on the filters sent: SEARCHSTRING", (done) => {
-      chai
-        .request(app)
-        .get("/api/products?searchString=Nutella")
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res.status).to.be.equal(200);
-          expect(res.body).to.be.an("array");
-          expect(res.body.length).to.be.equal(2);
-          expect(res.body.map((product) => product.id)).to.include.members([
-            "000000000000000000000012",
-            "000000000000000000000006",
-          ]);
-          done();
-        });
-    });
-
-    it("it should retrieve the list of products based on the filters sent: ids", (done) => {
-      chai
-        .request(app)
-        .get(
-          "/api/products?ids=000000000000000000000001,000000000000000000000003,000000000000000000000008"
-        )
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res.status).to.be.equal(200);
-          expect(res.body).to.be.an("array");
-          expect(res.body.length).to.be.equal(3);
-          expect(res.body.map((product) => product.id)).to.include.members([
-            "000000000000000000000001",
-            "000000000000000000000003",
-            "000000000000000000000008",
-          ]);
-          done();
-        });
-    });
-
-    it("it should retrieve the list of products based on the filters sent: CATEGORY, SEARCHSTRING", (done) => {
-      chai
-        .request(app)
-        .get("/api/products?category=fruit&searchString=Italy")
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res.status).to.be.equal(200);
-          expect(res.body).to.be.an("array");
-          expect(res.body.length).to.be.equal(1);
-          expect(res.body.map((product) => product.id)).to.include.members([
-            "000000000000000000000001",
-          ]);
-          done();
-        });
-    });
-
-    it("If no filter are passed then all the products should be retrieved", (done) => {
-      chai
-        .request(app)
-        .get("/api/products")
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res.status).to.be.equal(200);
-          expect(res.body).to.be.an("array");
-          expect(res.body.length).to.be.equal(12);
-          done();
-        });
-    });
-
-    it("If there are no products with applied filters then it should return an empty array", (done) => {
-      chai
-        .request(app)
-        .get("/api/products?category=meat&searchString=Do not find")
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res.status).to.be.equal(200);
-          expect(res.body).to.be.an("array");
-          expect(res.body.length).to.be.equal(0);
-          done();
-        });
-    });
-
-    it("It should return an error if the ids passed are not valid: ", (done) => {
-      chai
-        .request(app)
-        .get(
-          "/api/products?ids=000000000000000000000001,000000000000000000000003,08"
-        )
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res.status).to.be.equal(400);
-          done();
-        });
-    });
-
-    it("It should return an error if the category passed is not valid: ", (done) => {
-      chai
-        .request(app)
-        .get("/api/products?category=pizza")
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res.status).to.be.equal(400);
-          done();
-        });
-    });
-
-    it("It should return an error while reading on the db if it has a wrong category: ", (done) => {
-      //upload new data set:
-      mongoUnit.drop();
-      dao.close();
-
-      dao.open();
-      mongoUnit.load(testData.productsCollectionWithCategoryError);
-      dao.createProductsTextSearchIndexes();
-      chai
-        .request(app)
-        .get("/api/products")
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res.status).to.be.equal(500);
           done();
         });
     });

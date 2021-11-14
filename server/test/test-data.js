61cd9cf504d94ae1e37f616875ea665a895812b2
G-Salviati <giovannisalviati@gmail.com>
2021-11-14 16:57:12 +0100

    removed password prop in client test-data

--- server/test/test-data.js
+++ server/test/test-data.js
@@ -23,7 +23,6 @@ exports.clientsCollection = {
     {
       _id: ObjectID("6187c957b288576ca26f8257"),
       email: "client1@test.com",
-      password: "sosuonareeh?",
       fullName: " Domenico Bini",
       phoneNumber: 3205708803,
       address: "via Domenico Bini,26 Torino,10538",
@@ -32,7 +31,6 @@ exports.clientsCollection = {
     {
       _id: ObjectID("6a8fc927bb88c762a26f0000"),
       email: "client2@test.com",
-      password: "sonouncriticoeh?",
       fullName: "Andrea Diprè",
       phoneNumber: 3205755555,
       address: "via Andrea Dipre,24 Torino,10538",

f69ad8281426e5c525a9e86a3862e9e8daf2c67f
Daniele Spina s285377 <danspi@live.it>
2021-11-29 13:08:39 +0100

    Revert "Check if github deletion problem"
    This reverts commit 3d38f0e8abf998e49585501181572149db58c07b.

--- server/app.js
+++ server/app.js
@@ -124,6 +124,13 @@ app.get(
   orderHandlers.getOrdersByClientID
 );
 
+app.patch(
+  buildAPIPath("/orders/:orderID/complete"),
+  orderHandlers.completeOrderValidatorChain,
+  checkValidationErrorMiddleware,
+  orderHandlers.completeOrderHandler
+);
+
 // ---------
 // /products
 // ---------

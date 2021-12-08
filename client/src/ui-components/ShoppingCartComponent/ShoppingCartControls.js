03b56ff7fc8b560049ec65c4f72241f33c4154a1
AndreaSR95 <56077426+AndreaSR95@users.noreply.github.com>
2021-12-09 00:39:07 +0100

    button width fix

--- client/src/ui-components/ShoppingCartComponent/ShoppingCartControls.js
+++ client/src/ui-components/ShoppingCartComponent/ShoppingCartControls.js
@@ -2,30 +2,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./ShoppingCartControlsCSS.css";
 import React from "react";
 import Button from "../Button/Button";
-import { Link } from "react-router-dom";
-import { Col, Row} from "react-bootstrap";
-
+import {Row} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
 
 function ShoppingCartControls(props) {
+  const history = useHistory();
+
   return (
+    <>
     <Row>
-      <Col>
-        <Link
-          to={{
-            pathname: "/",
-            state: { shoppingCart: props.cart, clientID: props.clientID },
-          }}>
-          <Button className="cart-button-shopping" variant='light'>
-            CONTINUE SHOPPING
-          </Button>
-        </Link>
-      </Col>
-      <Col>
-        <Button className="cart-button-placeorder" disabled={props.cart.size === 0} onClick={props.handleShow}>
-          PLACE ORDER
-        </Button>
-      </Col>
+      <Button onClick={()=>{history.push("/", { shoppingCart: props.cart, clientID: props.clientID })}} className="cart-button-shopping" variant='light'>
+        CONTINUE SHOPPING
+      </Button>
     </Row>
+    <Row>
+      <Button className="cart-button-placeorder" disabled={props.cart.size === 0} onClick={props.handleShow}>
+        PLACE ORDER
+      </Button>
+    </Row>
+    </>
   );
 }
 

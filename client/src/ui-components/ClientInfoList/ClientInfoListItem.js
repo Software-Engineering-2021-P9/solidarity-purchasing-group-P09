2592556ad63ac0d18ca5c31e8aa1bf7a0e910509
AdrianaProvenzano <adriana.prove97@gmail.com>
2021-12-10 16:05:47 +0100

    updated responsiveness in ClientManagementPage

--- client/src/ui-components/ClientInfoList/ClientInfoListItem.js
+++ client/src/ui-components/ClientInfoList/ClientInfoListItem.js
@@ -4,12 +4,18 @@ import "./ClientInfoListItem.css";
 
 function ClientInfoListItem(props) {
   return (
-    <Container className='client-info-list-item py-3' onClick={props.onClick}>
-      <Row className='align-items-center'>
-        <Col>{props.clientInfo.fullName}</Col>
-        <Col>{props.clientInfo.address}</Col>
-        <Col>{props.clientInfo.phoneNumber}</Col>
-        <Col className='client-info-list-item-arrow'>
+    <Container className="client-info-list-item px-4" onClick={props.onClick}>
+      <Row className="py-2">
+        <Col className="info px-4">
+          <Row className="client-name mb-2">{props.clientInfo.fullName}</Row>
+          <Row>{props.clientInfo.email}</Row>
+          <Row>{props.clientInfo.phoneNumber}</Row>
+          <Row>{props.clientInfo.address}</Row>
+        </Col>
+        <Col
+          className="client-info-list-item-arrow d-flex align-items-center"
+          sm="1"
+        >
           <FaArrowRight />
         </Col>
       </Row>

36d8784b00d0ffd0d42d71cecd07a940ce20e523
AdrianaProvenzano <adriana.prove97@gmail.com>
2021-12-12 11:39:17 +0100

    update responsiveness in ClientOrders

--- client/src/ui-components/ClientOrdersComponent/ClientOrderTableRow.js
+++ client/src/ui-components/ClientOrdersComponent/ClientOrderTableRow.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
 import { PopUpForCompleteOrder } from "./PopUpForCompleteOrder";
 import "./ClientOrderTableRow.css";
-import Button from "../Button/Button";
 import { updateStatus } from "../../services/ApiClient";
 import Order from "../../services/models/Order";
+import {
+  calendarIcon,
+  cashIcon,
+  listIcon,
+  pinMapSmallIcon,
+  statusIcon,
+} from "../icon";
+import dayjs from "dayjs";
+
 function ClientOrderTableRow(props) {
   const [status, setStatus] = useState(props.order.status);
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -15,35 +24,107 @@ function ClientOrderTableRow(props) {
   };
 
   return (
-    <>
+    <Row className="my-3 container-sm mx-0 px-0">
       <PopUpForCompleteOrder
         handleSubmit={handleSubmit}
         modalIsOpen={modalIsOpen}
         setModalIsOpen={setModalIsOpen}
       />
-      <tr className='border-bottom'>
-        <td className='table-row'>{props.order.id}</td>
-        <td className='table-row'>{props.order.totalPrice}€</td>
-        <td className='table-row'> {props.order.createdAt}</td>
-        <td className='table-row'> {props.order.location}</td>
-        {status === Order.OrderStatus.PREPARED ? (
-          <td className='table-row-status-prepared'>{status}</td>
-        ) : (
-          <td className='table-row-status-done'>{status}</td>
-        )}
-
-        {status === Order.OrderStatus.PREPARED ? (
-          <td>
-            <Button onClick={() => setModalIsOpen(true)}>
+      <Row className="mx-0 px-0 align-items-center table-cell-visibility">
+        <Col md="3" lg="3" xl="3" className="mx-0 px-0">
+          {props.order.id}
+        </Col>
+        <Col className="mx-0 px-0">{props.order.totalPrice} €</Col>
+        <Col md="2" lg="2" xl="2" className="mr-0 pr-0">
+          {dayjs(props.order.createdAt).format("DD/MM/YYYY  hh:mm")}
+        </Col>
+        <Col md="3" lg="3" xl="3" className="mx-0 px-0">
+          {props.order.location}
+        </Col>
+        <Col md="3" lg="3" xl="3" className="mx-0 px-0">
+          <Row>
+            <Col className="d-flex align-items-center">
+              {status === Order.OrderStatus.DONE ? (
+                <span className="table-row-status-done mx-0 px-0">
+                  {status}
+                </span>
+              ) : (
+                <span className="table-row-status-prepared mx-0 px-0">
+                  {status}
+                </span>
+              )}
+            </Col>
+            <Col md="auto" lg="auto" xl="auto">
+              {status === Order.OrderStatus.PREPARED && (
+                <span className="d-flex justify-content-end">
+                  <span
+                    className="px-1 change-status"
+                    onClick={() => setModalIsOpen(true)}
+                  >
+                    Change Status
+                  </span>
+                </span>
+              )}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="mx-0 px-0 table-cell-visibility-small">
+        <Col md="12">
+          <Row>
+            <Col sm="auto" xs="auto">
+              {listIcon}
+            </Col>
+            <Col>{props.order.id}</Col>
+          </Row>
+          <Row>
+            <Col sm="auto" xs="auto">
+              {cashIcon}
+            </Col>
+            <Col>{props.order.totalPrice} €</Col>
+          </Row>
+          <Row>
+            <Col sm="auto" xs="auto">
+              {calendarIcon}
+            </Col>
+            <Col>
               {" "}
-              Change Status{" "}
-            </Button>
-          </td>
-        ) : (
-          <td className='table-row'></td>
-        )}
-      </tr>
-    </>
+              {dayjs(props.order.createdAt).format("DD/MM/YYYY  hh:mm")}
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="auto" xs="auto">
+              {pinMapSmallIcon}{" "}
+            </Col>
+            <Col> {props.order.location}</Col>
+          </Row>
+          <Row>
+            <Col className="d-flex align-items-center" sm="auto" xs="auto">
+              {statusIcon}
+            </Col>
+            {status === Order.OrderStatus.DONE ? (
+              <Col className="table-row-status-done d-flex align-items-center">
+                {status}
+              </Col>
+            ) : (
+              <Col className="table-row-status-prepared d-flex align-items-center">
+                {status}
+              </Col>
+            )}
+            {status === Order.OrderStatus.PREPARED && (
+              <Col className="d-flex justify-content-end">
+                <span
+                  className="px-1 change-status"
+                  onClick={() => setModalIsOpen(true)}
+                >
+                  Change Status
+                </span>
+              </Col>
+            )}
+          </Row>
+        </Col>
+      </Row>
+    </Row>
   );
 }
 

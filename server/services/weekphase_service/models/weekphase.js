1e8cca7c4930b7514922913736ce51d8d47fce64
MarcoBramini <braminim@gmail.com>
2021-12-13 10:55:55 +0100

    Fix timezones

--- server/services/weekphase_service/models/weekphase.js
+++ server/services/weekphase_service/models/weekphase.js
@@ -1,17 +1,24 @@
 "use strict";
 
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 class WeekPhase {
-  constructor(id, from, to, handler) {
+  constructor(id, from, to, timezone, handler) {
     this.id = id;
     this.from = from;
     this.to = to;
+    this.timezone = timezone;
     this.handler = handler;
   }
 
   // Checks if a date is within the weekphase boundaries.
   // Input date must be a DayJS object.
   containsDate(date) {
-    let weekTimeToCheck = date.format("dHHmm");
+    let weekTimeToCheck = date.tz(this.timezone).format("dHHmm");
     return weekTimeToCheck >= this.from && weekTimeToCheck < this.to;
   }
 }

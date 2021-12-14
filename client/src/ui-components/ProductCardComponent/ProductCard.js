eb19612c108b36e45e726d007fa2e56289049827
AdrianaProvenzano <adriana.prove97@gmail.com>
2021-12-14 10:43:17 +0100

    empty commit

--- client/src/ui-components/ProductCardComponent/ProductCard.js
+++ client/src/ui-components/ProductCardComponent/ProductCard.js
@@ -22,6 +22,7 @@ import ImageService from "../../services/ImageService/ImageService";
 
 function ProductCard(props) {
   const product = props.product;
+
   let left_availability = product.availability.leftQuantity; //mock left_availability
   let dropdown_items = [];
   for (let i = 1; i < 11; i++) {

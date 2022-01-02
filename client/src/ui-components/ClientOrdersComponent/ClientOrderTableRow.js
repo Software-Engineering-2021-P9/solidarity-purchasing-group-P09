357782a51fce7e31d3e7a9fe479c19d1cdee3af1
AndreaSR95 <56077426+AndreaSR95@users.noreply.github.com>
2022-01-02 01:42:51 +0100

    updated with main branch changes

--- client/src/ui-components/ClientOrdersComponent/ClientOrderTableRow.js
+++ client/src/ui-components/ClientOrdersComponent/ClientOrderTableRow.js
@@ -15,39 +15,39 @@ import {
 import dayjs from "dayjs";
 import UserRoles from "../../services/models/UserRoles";
 
-function setupStatusLabelAndRowButton(location, status, setModalIsOpen){
-  let tableRowComponents = {
-    statusLabel: <td className='table-row-status-not-covered'>{"invalid status"}</td>,
-    rowButton: <td className='table-row'></td>
-  };
+// function setupStatusLabelAndRowButton(location, status, setModalIsOpen){
+//   let tableRowComponents = {
+//     statusLabel: <td className='table-row-status-not-covered'>{"invalid status"}</td>,
+//     rowButton: <td className='table-row'></td>
+//   };
 
-  switch(status){
-    case(Order.OrderStatus.PREPARED):
-      tableRowComponents.statusLabel = <td className='table-row-status-prepared'>{status}</td>;
-      if(location !== "/NotCoveredOrders")
-        tableRowComponents.rowButton = 
-          <td className='table-row'>
-            <Button onClick={() => setModalIsOpen(true)}>Change Status</Button>
-          </td>
-      break;
+//   switch(status){
+//     case(Order.OrderStatus.PREPARED):
+//       tableRowComponents.statusLabel = <td className='table-row-status-prepared'>{status}</td>;
+//       if(location !== "/NotCoveredOrders")
+//         tableRowComponents.rowButton = 
+//           <td className='table-row'>
+//             <Button onClick={() => setModalIsOpen(true)}>Change Status</Button>
+//           </td>
+//       break;
 
-    case(Order.OrderStatus.NOT_COVERED):
-      tableRowComponents.statusLabel = <td className='table-row-status-not-covered'>{status}</td>; 
-      break;
+//     case(Order.OrderStatus.NOT_COVERED):
+//       tableRowComponents.statusLabel = <td className='table-row-status-not-covered'>{status}</td>; 
+//       break;
     
-      case(Order.OrderStatus.DONE):
-      tableRowComponents.statusLabel = <td className='table-row-status-done'>{status}</td>; 
-      break;
+//       case(Order.OrderStatus.DONE):
+//       tableRowComponents.statusLabel = <td className='table-row-status-done'>{status}</td>; 
+//       break;
 
-      case(Order.OrderStatus.WAITING):
-      tableRowComponents.statusLabel = <td className='table-row-status-waiting'>{status}</td>; 
-      break;
+//       case(Order.OrderStatus.WAITING):
+//       tableRowComponents.statusLabel = <td className='table-row-status-waiting'>{status}</td>; 
+//       break;
 
-    default:
-      break;
-  }
-  return tableRowComponents;
-}
+//     default:
+//       break;
+//   }
+//   return tableRowComponents;
+// }
 
 
 function ClientOrderTableRow(props) {
@@ -63,7 +63,7 @@ function ClientOrderTableRow(props) {
     setModalIsOpen(false);
   };
 
-  let tableRowComponents = setupStatusLabelAndRowButton(history.location.pathname, status, setModalIsOpen);
+  //let tableRowComponents = setupStatusLabelAndRowButton(history.location.pathname, status, setModalIsOpen);
 
   return (
     <Row className="my-3 px-0">

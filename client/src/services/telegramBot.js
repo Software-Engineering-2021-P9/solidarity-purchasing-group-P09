970edda430bb8180e9815118c9323707440d2e94
Ehsan Ansari Nejad <s288903@studenti.polito.it>
2022-01-06 17:14:06 +0100

    Update telegramBot.js

--- client/src/services/telegramBot.js
+++ client/src/services/telegramBot.js
@@ -2,10 +2,6 @@ const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 let users = [];
-//bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 //Client sends start and will receive a welcome message,
 //we can keep users ID to use them later with users.push(ctx.chat.id);
 bot.command("start", (ctx) => {

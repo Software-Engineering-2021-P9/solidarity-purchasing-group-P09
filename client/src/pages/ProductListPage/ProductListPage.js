afc19a9b4dd205a1ad2642a78c7f3acc7b185917
AdrianaProvenzano <adriana.prove97@gmail.com>
2022-01-06 17:28:49 +0100

    empty commit

--- client/src/pages/ProductListPage/ProductListPage.js
+++ client/src/pages/ProductListPage/ProductListPage.js
@@ -9,10 +9,10 @@ import ProductCard from "../../ui-components/ProductCardComponent/ProductCard";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./ProductListPage.css";
-import { alertIcon } from "../../ui-components/icons"
+import { alertIcon } from "../../ui-components/icons";
 
 import { findProducts, getOrders } from "../../services/ApiClient";
-import  Order  from "../../services/models/Order"
+import Order from "../../services/models/Order";
 
 import { AuthContext } from "../../contexts/AuthContextProvider";
 import UserRoles from "../../services/models/UserRoles";
@@ -39,20 +39,30 @@ function ProductListPage(props) {
   );
 
   const [cartUpdated, setCartUpdated] = useState(false);
-
   const [cartEmpty, setCartEmpty] = useState(false);
 
   const currentUser = authContext.currentUser;
   const [firstTimeNotify, setFirstTimeNotify] = useState(true);
-  const [clientHasNotCoveredOrders, setClientHasNotCoveredOrders] = useState(false);
+  const [clientHasNotCoveredOrders, setClientHasNotCoveredOrders] =
+    useState(false);
 
-  if(clientHasNotCoveredOrders === false && currentUser !== null && currentUser.role === "client"){
-    getOrders(currentUser.id).then(orders=>{
-      if(orders.filter(order=>order.status === Order.OrderStatus.NOT_COVERED).length > 0)
-        setClientHasNotCoveredOrders(true);
-    }).catch(err=>{
-      throw(err);
-    });
+  if (
+    clientHasNotCoveredOrders === false &&
+    currentUser !== null &&
+    currentUser.role === "client"
+  ) {
+    getOrders(currentUser.id)
+      .then((orders) => {
+        if (
+          orders.filter(
+            (order) => order.status === Order.OrderStatus.NOT_COVERED
+          ).length > 0
+        )
+          setClientHasNotCoveredOrders(true);
+      })
+      .catch((err) => {
+        throw err;
+      });
   }
 
   useEffect(() => {
@@ -76,7 +86,7 @@ function ProductListPage(props) {
       }, 3000);
     }
   }, [cartUpdated, cartEmpty]);
-  
+
   useEffect(() => {
     //call from props the function for fetching the new products
     async function updateProducts() {
@@ -166,24 +176,56 @@ function ProductListPage(props) {
           })}
       </Row>
 
-      <Modal centered show={currentUser !== null && currentUser.role === "client" && clientHasNotCoveredOrders && firstTimeNotify}>
+      <Modal
+        centered
+        show={
+          currentUser !== null &&
+          currentUser.role === "client" &&
+          clientHasNotCoveredOrders &&
+          firstTimeNotify
+        }
+      >
         <Modal.Header>
           <div className="margin-auto center-text">
-            <Modal.Title className="margin-auto" > {alertIcon} Insufficient Wallet Balance</Modal.Title>
+            <Modal.Title className="margin-auto">
+              {" "}
+              {alertIcon} Insufficient Wallet Balance
+            </Modal.Title>
           </div>
         </Modal.Header>
 
         <Modal.Body>
-          <p className="center-text">At the moment there are orders with status "not covered", waiting for a wallet top up.</p>
-          <p className="center-text">Not covered orders will be deleted at the defined deadline.</p>
+          <p className="center-text">
+            At the moment there are orders with status "not covered", waiting
+            for a wallet top up.
+          </p>
+          <p className="center-text">
+            Not covered orders will be deleted at the defined deadline.
+          </p>
         </Modal.Body>
 
         <Modal.Footer>
           <div className="margin-auto">
-            <Button className="product-list-page-popup-buttons" variant="primary" onClick={()=>{setFirstTimeNotify(false)}} >Close</Button>
+            <Button
+              className="product-list-page-popup-buttons"
+              variant="primary"
+              onClick={() => {
+                setFirstTimeNotify(false);
+              }}
+            >
+              Close
+            </Button>
           </div>
-            <div className="margin-auto">
-            <Button className="product-list-page-popup-buttons" variant="primary" onClick={()=>{history.push("/client")}}>Check Orders</Button>
+          <div className="margin-auto">
+            <Button
+              className="product-list-page-popup-buttons"
+              variant="primary"
+              onClick={() => {
+                history.push("/client");
+              }}
+            >
+              Check Orders
+            </Button>
           </div>
         </Modal.Footer>
       </Modal>

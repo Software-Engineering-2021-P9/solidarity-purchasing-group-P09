63b75fada4981b9db5f7885a0aca99f803fd8e5a
AdrianaProvenzano <adriana.prove97@gmail.com>
2022-01-07 18:42:37 +0100

    fixed warnings in icons

--- client/src/ui-components/icons.js
+++ client/src/ui-components/icons.js
@@ -347,7 +347,7 @@ const gridIcon = (
     width="32"
     height="32"
     fill="currentColor"
-    class="bi bi-ui-radios-grid"
+    className="bi bi-ui-radios-grid"
     viewBox="0 0 16 16"
   >
     <path d="M3.5 15a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm9-9a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm0 9a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zM16 3.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0zm-9 9a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0zm5.5 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zm-9-11a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 2a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
@@ -360,11 +360,11 @@ const arrowDownIcon = (
     width="24"
     height="24"
     fill="green"
-    class="bi bi-arrow-down"
+    className="bi bi-arrow-down"
     viewBox="0 0 16 16"
   >
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
     />
   </svg>
@@ -376,11 +376,11 @@ const arrowUpIcon = (
     width="24"
     height="24"
     fill="red"
-    class="bi bi-arrow-up"
+    className="bi bi-arrow-up"
     viewBox="0 0 16 16"
   >
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
     />
   </svg>

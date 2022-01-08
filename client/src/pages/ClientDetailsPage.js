77fabd58559a3a0eab7efd37fc3fb2e44a3f1e21
MrGreensushi <danspi@live.it>
2022-01-08 12:25:23 +0100

    Sort BE

--- client/src/pages/ClientDetailsPage.js
+++ client/src/pages/ClientDetailsPage.js
@@ -65,11 +65,7 @@ function ClientDetailsPage(props) {
 
           //fetch orders:
           getOrders(result.id).then((newO) => {
-            const sortedOrders = newO.sort(
-              (newer, older) =>
-                new Date(newer.createdAt) < new Date(older.createdAt)
-            );
-            setOrders(sortedOrders);
+            setOrders(newO);
           });
         })
         .catch((err) =>

63dcc306e28a38726e1b18746bd8277cc7024975
AndreaSR95 <56077426+AndreaSR95@users.noreply.github.com>
2022-01-10 22:41:17 +0100

    s

--- server/test/test.js
+++ server/test/test.js
@@ -1002,7 +1002,7 @@ describe("Clients API tests:", () => {
           expect(err).to.be.null;
           expect(res.status).to.be.equal(200);
           expect(res.body).to.be.an("object");
-          expect(res.body.newWalletValue).to.be.equal(67.5);
+          expect(res.body.newWalletValue).to.be.equal(0);
 
           done();
         });

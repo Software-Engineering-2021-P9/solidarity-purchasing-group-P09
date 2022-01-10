8c05421d351ca10c98edb717d63f4a8ffa53bf8b
G-Salviati <63968840+G-Salviati@users.noreply.github.com>
2022-01-10 21:02:16 +0100

    Adds week trigger friday (#102)

--- server/services/weekphase_service/weekphase_config.js
+++ server/services/weekphase_service/weekphase_config.js
@@ -2,6 +2,8 @@
 
 const dayjs = require("dayjs");
 const { WeekPhase } = require("./models/weekphase");
+var dao = require("../../dao/dao");
+const { getCurrentWeekClient } = require("../time_service");
 
 // Weekphases configuration
 // No overlapping phases are allowed
@@ -60,7 +62,7 @@ exports.weekphasesConfig = [
     "52200",
     "60900",
     "Europe/Rome",
-    weekphaseEightHandler
+    exports.weekphaseEightHandler
   ),
   new WeekPhase(
     "weekphase-9",
@@ -178,9 +180,17 @@ function weekphaseSevenHandler() {
 // Employee:
 //    - Can make orders (NWC)
 //    - Can update pickup time (NWC)
-function weekphaseEightHandler() {
+exports.weekphaseEightHandler = async () => {
   console.log("weekphase-8", dayjs().toISOString());
-}
+
+  try {
+    await dao.setPreparedOrdersToUnretrieved(...getCurrentWeekClient());
+  } catch (err) {
+    console.log(
+      `WeekphaseEightHandler() --> Couldn't set current week prepared orders to unretrieved: ${err}`
+    );
+  }
+};
 
 // - Weekphase 9 (SAT 09:00 - SUN 00:00):
 // Farmers:
